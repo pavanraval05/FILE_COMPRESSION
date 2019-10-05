d98e41ad1189da5353148b80b21beed6e18b5ae4
kishan patel <kp768919@gmail.com>
2019-10-05 16:28:35 +0530

    huffman basic structure created

---
+++ src/huffman_compression.h
@@ -0,0 +1,30 @@
+/* File contains the prototypes of functions required
+ * for creating a huffman tree. 
+ */
+typedef struct huffman_tree_element {
+    char ch;
+    long int frequency;
+} tree_elements;  
+
+void compress_by_huffman(char *fil_name);
+
+/* A effient sorting algorihtm will be required inorder
+ * to find the frequency of the characters.
+ */
+char *sort_file(char *file_name);
+
+void Qsort_file(char *arr, long int left, long int right);
+
+void swap_chars(char *a, char *b);
+
+void Qsort_tree(tree_elements *arr, int left, int right);
+
+void swap_tree_el(tree_elements *a, tree_elements *b);
+
+tree_elements *get_tree_elements(char *arr, long int get_dist_chars);
+
+long int get_dist_chars(char *arr);
+
+tree_elements get_next_token(char *arr);
+
+
